7656ce579ee914392acc74e284c241f8ab9ee60f
Schalk Venter <venterschalk@gmail.com>
2025-08-04 20:28:35 +0200

    Add Vercel adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic()],
+  adapter: vercel(),
 });
